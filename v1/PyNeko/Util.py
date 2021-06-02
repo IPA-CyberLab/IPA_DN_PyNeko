d1db7b45992d944f82411683fefdb538a59d7ed9
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-02 21:37:24 +0900

    途中さん

--- v1/PyNeko/Util.py
+++ v1/PyNeko/Util.py
@@ -12,6 +12,109 @@ import subprocess
 
 from ._Imports import *
 
+class Str:
+    @staticmethod
+    def IsNull(str:str)->bool:
+        return Util.IsNull(str)
+
+    @staticmethod
+    def IsNonNull(str:str)->bool:
+        return not IsNull(str)
+    
+    @staticmethod
+    def ToStr(str: any) -> str:
+        if (Util.IsNull(str)):
+            return ""
+
+        if Util.IsType(str, "str"):
+            print("a")
+            return str
+        
+        return F"{str}"
+
+    @staticmethod
+    def NonNull(str: str) -> str:
+        if Str.IsNull(str):
+            return ""
+        
+        return str
+
+    @staticmethod
+    def StrToInt(str: str) -> int:
+        try:
+            if Str.IsNull(str):
+                return 0
+            i = int(str)
+            return i
+        except:
+            return 0
+
+    @staticmethod
+    def Trim(str: str) -> str:
+        return Str.NonNull(str).strip()
+
+    @staticmethod
+    def IsEmpty(str: str) -> bool:
+        if Str.Len(Str.Trim(str)) == 0:
+            return True
+        return False
+    
+    @staticmethod
+    def IsFilled(str: str) -> bool:
+        return not Str.IsEmpty(str)
+    
+    @staticmethod
+    def Len(str: str) -> int:
+        if Str.IsNull(str):
+            return 0
+        
+        return len(str)
+
+    @staticmethod
+    def ToBool(str: str) -> bool:
+        i = Str.StrToInt(str)
+        if i != 0:
+            return True
+        
+        tmp = Str.Trim(str).lower()
+
+        if Str.Len(tmp) >= 1:
+            if tmp[0] == 'y' or tmp[0] == 't':
+                return True
+            if tmp.startswith("ok") or tmp.startswith("on") or tmp.startswith("enable"):
+                return True
+
+        return False
+
+class Util:
+    @staticmethod
+    def ToBool(object: any) -> bool:
+        if Util.IsType(object, "str"):
+            return Str.ToBool(str)
+
+        if not (object):
+            return False
+
+        return True
+    
+    @staticmethod
+    def IsNull(object: any) -> bool:
+        if object == None:
+            return True
+
+        return False
+
+    @staticmethod
+    def GetTypeName(object: any) -> str:
+        return type(object).__name__
+    
+    @staticmethod
+    def IsType(object: any, typeName: str) -> bool:
+        if Util.GetTypeName(object) == typeName:
+            return True
+        
+        return False
+
 
 class EasyExecResults:
     Result: subprocess.CompletedProcess = None
@@ -34,7 +137,6 @@ class EasyExecResults:
 
 
 class EasyExec:
-
     @staticmethod
     def ShellExecute(command: str, ignoreError: bool = False, timeoutSecs: int = None):
         res = subprocess.run(command, shell=True, encoding="utf-8", text=True, timeout=timeoutSecs)
