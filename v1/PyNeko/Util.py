46dd5c1164be65743f5980d323781494806a9c83
Daiyuu Nobori <dnobori.gitcommit@coe.ad.jp>
2021-06-02 21:04:26 +0900

    途中さん

--- v1/PyNeko/Util.py
+++ v1/PyNeko/Util.py
@@ -13,15 +13,47 @@ import subprocess
 from ._Imports import *
 
 
+class EasyExecResults:
+    Result: subprocess.CompletedProcess = None
+    IsOk: bool = False
+    StdOut: str = ""
+    StdErr: str = ""
+    StdOutAndErr: str = ""
+    ExitCode: int = -1
 
-class Process:
+    def InitFromCompletedProcess(self, res: subprocess.CompletedProcess):
+        self.Result = res
+        self.ExitCode = res.returncode
+        self.IsOk = (res.returncode == 0)
+        self.StdOut = str(res.stdout)
+        self.StdErr = str(res.stderr)
+        self.StdOutAndErr = self.StdOut + "\n" + self.StdErr + "\n"
+
+    def ThrowIfError(self):
+        self.Result.check_returncode()
+
+
+class EasyExec:
 
     @staticmethod
-    # シェルコマンドの実行
-    def ShellExecute(command: str, ignoreError: bool = False):
-        res = subprocess.run(command, shell=True, encoding="utf-8", text=True)
+    def ShellExecute(command: str, ignoreError: bool = False, timeoutSecs: int = None):
+        res = subprocess.run(command, shell=True, encoding="utf-8", text=True, timeout=timeoutSecs)
 
         if not ignoreError:
             res.check_returncode()
 
+    @staticmethod
+    def ShellExecutePiped(command: str, ignoreError: bool = False, timeoutSecs: int = None) -> EasyExecResults:
+        res = subprocess.run(command, shell=True, encoding="utf-8", text=True, timeout=timeoutSecs,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 
+        ret = EasyExecResults()
+        ret.InitFromCompletedProcess(res)
+
+        if not ignoreError:
+            ret.ThrowIfError()
+        
+        return ret
+
+
+
+ 
